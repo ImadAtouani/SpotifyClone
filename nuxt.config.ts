6c3bf067b48b89773c4712b03527bf15661bf33b
Imad Atouani <149882795+ImadAtouani@users.noreply.github.com>
2025-05-10 17:11:06 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   ssr: false,
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-05-10',
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
       SPOTIFY_CLIENT_ID: "dfc84157e4164b81b559f0b8e29ec6a5",
       // SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
       SPOTIFY_CLIENT_SECRET: "75462c0bb0bb4e2cbb7df4db5be15b8e",
-      BASE_URL: 'https://example.com/callback'
+      BASE_URL: 'https://spotify-clone2025.netlify.app/callback'
     }
   },
   modules: [
